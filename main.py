454fe443086469082ef74b5104ada7e73eca67a8
VitaliiZZzz <54666246+VitaliiZZzz@users.noreply.github.com>
2021-11-02 20:09:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import roman
+
+
+def task1():
+    data = pd.read_csv('Birthweight.csv')
+    subData = np.array(data['Gestation'])
+    subMassive = pd.Series(subData, index=[roman.toRoman(i + 1) for i in range(len(subData))])
+    print(subMassive)
+
+    print('\nЗвернення за прямим індексом:\n{}'. format(subMassive.loc['XXXIV':'XL']))
+    print('\nЗвернення за прямим індексом:\n{}'.format(subMassive[33:40]))
+
+task1()
