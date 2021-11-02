aac316ecd5c3af6d8d1e9279655d7e769742297c
wladziaya <66369788+wladziaya@users.noreply.github.com>
2021-11-02 23:22:01 +0200

    add task4

--- main.py
+++ main.py
@@ -9,12 +9,42 @@ def task1():
     subMassive = pd.Series(subData, index=[roman.toRoman(i + 1) for i in range(len(subData))])
     print(subMassive)
 
-    print('\nЗвернення за прямим індексом:\n{}'. format(subMassive.loc['XXXIV':'XL']))
+    print('\nЗвернення за прямим індексом:\n{}'.format(subMassive.loc['XXXIV':'XL']))
     print('\nЗвернення за прямим індексом:\n{}'.format(subMassive[33:40]))
+
 
 def task2():
     data = pd.read_csv('Birthweight.csv')
-    data['ratio'] = round(data['Length']/data['Birthweight'], 2)
+    data['ratio'] = round(data['Length'] / data['Birthweight'], 2)
     print(data)
+
+
+def task4():
+    data1 = pd.DataFrame({'motherCity': ['A', 'B', 'C', 'D'], 'childcount': [100000, 24653, 355223, 453234]})
+    data2 = pd.DataFrame({'motherCity': ['A', 'C', 'B', 'D'], 'medyears': [10, 12, 17, 12]})
+    merged_data = pd.merge(data1, data2)
+    data3 = pd.DataFrame({'motherCity': ['D', 'F', 'G', 'H'], 'medyears': [10, 12, 17, 12]})
+    data4 = pd.DataFrame({'medyears': [10, 17, 12], 'mapweight': ['30', '15', '44']})
+    data5 = pd.DataFrame({'medyears': [10, 17, 17, 10, 12], 'agedifference': [5, 2, 6, 18, 3]})
+    data6 = pd.DataFrame({'motherTown': ['A', 'C', 'D', 'B'], 'fweight': [67, 89, 78, 120]})
+    data7 = pd.DataFrame({'motherCity': ['A', 'B', 'C', 'D'], 'childcount': [980700, 38863, 246778, 678904]})
+
+    print('\nЗ\'єднання «один-до-одного»\n', pd.merge(data1, data2))
+    print('\nЗ\'єднання «багато-до-одного»\n', pd.merge(merged_data, data4))
+    print('\nЗ\'єднання «багато-до-багатьох»\n', pd.merge(merged_data, data5))
+    print('\nЗ\'єднання при різній назвій стовпців\n',
+          pd.merge(data1, data6, left_on='motherCity', right_on='motherTown'))
+    print('\nВидалення спільного стовпця\n',
+          pd.merge(data1, data6, left_on='motherCity', right_on='motherTown').drop('motherTown', axis=1))
+    print('\nInner\n', pd.merge(data1, data3))
+    print('\nOuter\n', pd.merge(data1, data3, how='outer'))
+    print('\nLeft\n', pd.merge(data1, data3, how='left'))
+    print('\nRight\n', pd.merge(data1, data3, how='right'))
+    print('\nОднакові стовпці за якими не відбувається злиття, додаються суфікси\n',
+          pd.merge(data1, data7, on='motherCity'))
+    print('\nДодамо суфікси самостійно\n', pd.merge(data1, data7, on='motherCity', suffixes=['_2001', '_2021']))
+
+
 task1()
 task2()
+task4()
